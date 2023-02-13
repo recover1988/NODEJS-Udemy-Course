1a622a5424994b003ae9476511114d85f2f6f5b4
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-13 17:55:03 -0300

    add dotenv

--- libs/postgres.js
+++ libs/postgres.js
@@ -3,7 +3,7 @@ const { config } = require("../config/config");
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbHost}${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const pool = new Pool({ connectionString: URI });
 
