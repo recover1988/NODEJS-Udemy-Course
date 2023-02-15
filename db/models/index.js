0ec695136278acd5d7b757d1a6cd9e3888298543
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-15 00:30:37 -0300

    add client model and relation one belong to many

---
+++ db/models/index.js
@@ -0,0 +1,11 @@
+const { User, UserSchema } = require("./user.model");
+const { Client, ClientSchema } = require("../models/client.model");
+
+function setupModels(sequelize) {
+  User.init(UserSchema, User.config(sequelize));
+  Client.init(ClientSchema, Client.config(sequelize));
+
+  Client.associate(sequelize.models)
+}
+
+module.exports = setupModels;
