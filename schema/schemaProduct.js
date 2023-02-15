a46f5d2ccb76d1b999fedc8ec05cbd4907e7f090
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-15 01:58:49 -0300

    schemaProduct with Joi

--- schema/schemaProduct.js
+++ schema/schemaProduct.js
@@ -1,20 +1,23 @@
 const Joi = require("joi");
 
-const id = Joi.string().uuid();
+const id = Joi.number().integer();
 const name = Joi.string().alphanum().min(3).max(10);
-const price = Joi.number().integer().min(2);
+const price = Joi.number().integer().min(10);
 const details = Joi.string().min(10);
+const categoryId = Joi.number().integer();
 
 const schemaProductCreate = Joi.object({
   name: name.required(),
   price: price.required(),
-  details: details.required(),
+  description: details.required(),
+  categoryId: categoryId.required(),
 });
 
 const updateSchemaProduct = Joi.object({
   name: name,
   price: price,
   details: details,
+  categoryId: categoryId,
 });
 
 const getProductSchema = Joi.object({
